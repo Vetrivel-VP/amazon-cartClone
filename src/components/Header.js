e41062302b54cd5e462a835e6de9af3da6cbc43c
VETRIVEL <vetrivel.galaxy@gmail.com>
2021-08-24 18:44:48 +0300

    Add : Header Component

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../css/Header.css";
+
+function Header() {
+  return (
+    <div className="App-header">
+      <h1>Amazon Cart</h1>
+    </div>
+  );
+}
+
+export default Header;
