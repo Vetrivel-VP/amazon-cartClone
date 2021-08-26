6f69d52cfc8db3cd81133f347dc80e4f9a8fe4da
VETRIVEL <vetrivel.galaxy@gmail.com>
2021-08-26 18:26:17 +0300

    Add: CartItems Data from Data.js

--- src/components/AppMain.js
+++ src/components/AppMain.js
@@ -1,12 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import "../css/App.css";
 import CartItems from "./CartItems.js";
 import CartTotal from "./CartTotal.js";
+import data from "../Data";
 
 function AppMain() {
+  const [cartItems, setCartItems] = useState(data);
   return (
     <div className="App-main">
-      <CartItems />
+      <CartItems items={cartItems} />
       <CartTotal />
     </div>
   );
